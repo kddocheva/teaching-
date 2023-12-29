b82c2091080df1c7816a1e85c21f557068b36270
Artur Kulig <hunteerq@icloud.com>
2023-12-29 15:16:40 +0100

    Initial lesson

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("Main")
+}
---
+++ src/main/kotlin/Student.kt
@@ -0,0 +1,1 @@
+data class Student(val name: String, val age: Int)
