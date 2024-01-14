34dba8a31b460d38e079acadba4ca24aaee6d14b
kristinadd <kristina@productboard.com>
2024-01-14 13:07:02 +0100

    hw done

--- src/main/kotlin/People.kt
+++ src/main/kotlin/People.kt
@@ -2,24 +2,23 @@ import java.io.File
 
 fun main() {
     val people = readFile()
-    people.forEach {
-        println(it)
-    }
-//    val avAge = avAge(people);
-//    println("Av. age = $avAge")
+    val avAge = avAge(people);
+    println("Av. age = $avAge")
 }
 
 fun readFile():List<Person> {
     val filePath = "people.csv"
 
-    val people = File(filePath).readLines().map { lines ->
+    val people = File(filePath).readLines()
+        .map { lines ->
             lines.split(",")
         }.map {
-            tokens -> Person(name = tokens[0], age = 20, gender = tokens[2], occupation = tokens[3], city = tokens[4])
-        }
+            tokens -> Person(name = tokens[0], age = tokens[1].toIntOrNull() ?: 0, gender = tokens[2], occupation = tokens[3], city = tokens[4])
+        }.drop(1)
     return people
 }
 
 fun avAge(people: List<Person>): Double {
-    TODO("calculate av. age for people")
+    return people.map { it.age }
+        .average()
 }
